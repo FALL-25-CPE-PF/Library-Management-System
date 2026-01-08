90f2d2990466ccb90b83e2f25f9768024cebc7e9
zainulabidin30 <zainzia239@gmail.com>
2026-01-08 00:34:58 -0800

    Update fines.py

--- src/fine.py
+++ src/fine.py
@@ -1,19 +1,29 @@
+# fines.py
 class FineSystem:
-    def calculate_fine(self, stype, days_issued, late_days, books):
-        rent_per_day = 10
-
-        # Rent calculation
-        rent = books * days_issued * rent_per_day
-
-        # Late fine calculation
+    def calculate(self, stype, days_issued, late_days, books_count):
+        # Rent per day for each student type
         if stype == "ug":
-            fine = late_days * 20
+            rent_per_day = 10
         elif stype == "pg":
-            fine = late_days * 10
+            rent_per_day = 15
         elif stype == "research":
-            fine = late_days * 10
+            rent_per_day = 20
         else:
-            fine = late_days * 30
-
+            rent_per_day = 10
+        
+        # Fine per day for late return
+        if stype == "ug":
+            fine_per_day = 20
+        elif stype == "pg":
+            fine_per_day = 10
+        elif stype == "research":
+            fine_per_day = 5
+        else:
+            fine_per_day = 20
+        
+        # Calculate charges
+        rent = books_count * days_issued * rent_per_day
+        fine = books_count * late_days * fine_per_day
         total = rent + fine
+        
         return rent, fine, total
